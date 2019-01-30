ee51abe30ea43601783bd8711d05d6ceeae0aff4
Whrabbit <whrabbitc@gmail.com>
2019-01-30 20:14:37 +0100

    a very pretty website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 import { MatToolbarModule } from "@angular/material";
-
+import { MatTabsModule } from '@angular/material';
 import { AppComponent } from "./app.component";
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatToolbarModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatToolbarModule, MatTabsModule, FlexLayoutModule],
   providers: [],
   bootstrap: [AppComponent]
 })
